ffd13095cc292ad65acb89d5cf7c35a75380c3c8
Mohammd <mohammadsaki92@yahoo.com>
2025-08-20 09:03:01 +0430

    Commit 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+
+//My first code in  react js
 createRoot.render(<h1>Hello, world!</h1>, document.getElementById('root'));
+
+// Now i want mack a Component
+class Time extends React.Component
+{
+  render()
+  {
+    return <h1>Current Time: {new Date().toLocaleTimeString()}</h1>;
+  } 
+}
+
+class App extends React.Component
+{
+  render()
+  {
+    return <div>
+      <h1>My First React App</h1>
+      <Time />
+    </div>;
+  }
+}
+createRoot.render(<App />, document.getElementById('root'));
